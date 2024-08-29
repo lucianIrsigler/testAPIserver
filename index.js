e914221e633778b1298c37f189a55126e3dbed5e
lucianIrsigler <l.irsigler64@gmail.com>
2024-08-29 13:57:37 +0200

    update

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get("/endpoint", (req, res) => {
     res.json({ message: "Hello, world!" });
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello")
+});
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
